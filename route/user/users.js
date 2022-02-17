c893b0ed07c503b5b66fbc4037c67c65ec9ee4ce
matheusnarciso@hotmail.com <matheusnarciso@hotmail.com>
2022-02-17 17:59:14 -0300

    v3 - montando recuperação de senha

--- route/user/users.js
+++ route/user/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Usuario = require("../../service/CtrlUsuario")
 
 router.post("/login", async (req, res) => {
-  const usuario = await Usuario.select(req.body.usuario.toUpperCase(), req.body.senha);
+  const usuario = await Usuario.select(req.body.email, req.body.senha);
 
   if (usuario.rows[0] !== undefined)
     return res.status(200).json({success:"true", user: usuario.rows[0]});
@@ -12,11 +12,17 @@ router.post("/login", async (req, res) => {
 })
 
 router.post("/forget", async (req, res) => {
-  return res.status(200).json({success:"true", user: null});
+  const usuario = await Usuario.select(req.body.email.toUpperCase());
+  if (usuario.rows[0] !== undefined)
+  {
+    //await Usuario.updateHash()
+    return res.status(200).json({success:"true", user: usuario.rows[0]});
+  } else
+    return res.status(200).json({success:"false", user: null});
 })
 
 router.post("/account", async (req, res) => {
-  const usuario = await Usuario.select(req.body.usuario.toUpperCase());
+  const usuario = await Usuario.select(req.body.email.toUpperCase());
 
   if (usuario.rows[0] !== undefined)
     return res.status(200).json({success:"true", user: usuario.rows[0]});
--- service/CtrlUsuario.js
+++ service/CtrlUsuario.js
@@ -1,27 +1,43 @@
 const oracledb = require('oracledb');
 const Oracle = require('./Oracle');
 
-const sql = `SELECT ID_USUARIO, NM_COMPLETO, E_MAIL, NM_USUARIO, DM_ATIVO, ID_EMPRESA FROM CTRL_USUARIO WHERE NM_USUARIO = :NM_USUARIO`;
-const sqlWhereSenha = ` AND SENHA_USUARIO = :SENHA_USUARIO`
+const sql = `SELECT ID_USUARIO, NM_COMPLETO, E_MAIL, NM_USUARIO, DM_ATIVO, HASH_RECOVERY FROM CTRL_USUARIO WHERE E_MAIL = :E_MAIL`;
+const sqlWhereSenha = ` AND SENHA_USUARIO_WEB = :SENHA_USUARIO_WEB`
 
-module.exports.select = async (usuario, senha = '') => {
+module.exports.select = async (email, senha = '') => {
   try {
     let qry, params;
 
     if (senha === ''){
       qry = sql;
       params = {
-        NM_USUARIO: usuario.toUpperCase(),
+        E_MAIL: email.toUpperCase(),
       };
     } else {
       qry = sql + sqlWhereSenha;
       params = {
-        NM_USUARIO: usuario.toUpperCase(),
-        SENHA_USUARIO: senha,
+        E_MAIL: email.toUpperCase(),
+        SENHA_USUARIO_WEB: senha,
       }
     }
 
     return await Oracle.select(qry, params);
+  
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+const sqlUpdate = `UPDATE CTRL_USUARIO SET HASH_RECOVERY = :HASH_RECOVERY WHERE ID_USUARIO = :ID_USUARIO`;
+
+module.exports.updateHash = async (id, hash) => {
+  try {
+    let params = {
+      HASH_RECOVERY: hash,
+      ID_USUARIO: id
+    };
+
+    return await Oracle.execute(qry, params, {autoCommit: true});
   
   } catch (err) {
     console.log(err.message);
